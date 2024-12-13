6e5d4b3739502d01132148e9a6bdd28b300cdc79
Henry Ho <101014484+hai-henry@users.noreply.github.com>
2024-12-13 16:07:51 -0600

    updated

---
+++ vim-options.lua
@@ -0,0 +1,14 @@
+vim.o.background = "dark"
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+
+
